9e6bf8c47998ba28131d2b60eb8bf9ae1ce5cbd9
Iftekhar16 <iftekharhossain16@gmail.com>
2023-06-21 19:55:03 +0600

    done

---
+++ calculations.js
@@ -0,0 +1,97 @@
+var count = 1;
+
+
+function calculateTriangle(){
+    let input1 = document.getElementById("triangle-input1").value;
+    let input2 = document.getElementById("triangle-input2").value;
+    if (isNaN(input1) || isNaN(input2) || input1=="" || input2=="") {
+        alert("Kindly enter a valid input");
+    }
+    else{
+        var result = (0.5*input1*input2).toFixed(2);
+        var p = document.createElement("p");
+        p.textContent = count+". Triangle: "+result+"cm2";
+        document.getElementById("result-results").appendChild(p);
+        count++;
+    }
+}
+
+
+function calculateRectangle(){
+    let input1 = document.getElementById("rectangle-input1").value;
+    let input2 = document.getElementById("rectangle-input2").value;
+    if (isNaN(input1) || isNaN(input2) || input1=="" || input2=="") {
+        alert("Kindly enter a valid input");
+    }
+    else{
+        var result = (input1*input2).toFixed(2);
+        var p = document.createElement("p");
+        p.textContent = count+". Rectangle: "+result+"cm2";
+        document.getElementById("result-results").appendChild(p);
+        count++;
+    }
+}
+
+
+function calculateParallelogram(){
+    let input1 = document.getElementById("parallelogram-input1").value;
+    let input2 = document.getElementById("parallelogram-input2").value;
+    if (isNaN(input1) || isNaN(input2) || input1=="" || input2=="") {
+        alert("Kindly enter a valid input");
+    }
+    else{
+        var result = (input1*input2).toFixed(2);
+        var p = document.createElement("p");
+        p.textContent = count+". Parallelogram: "+result+"cm2";
+        document.getElementById("result-results").appendChild(p);
+        count++;
+    }
+}
+
+
+function calculateRhombus(){
+    let input1 = document.getElementById("rhombus-input1").value;
+    let input2 = document.getElementById("rhombus-input2").value;
+    if (isNaN(input1) || isNaN(input2) || input1=="" || input2=="") {
+        alert("Kindly enter a valid input");
+    }
+    else{
+        var result = (0.5*input1*input2).toFixed(2);
+        var p = document.createElement("p");
+        p.textContent = count+". Rhombus: "+result+"cm2";
+        document.getElementById("result-results").appendChild(p);
+        count++;
+    }
+}
+
+
+function calculatePentagon(){
+    let input1 = document.getElementById("pentagon-input1").value;
+    let input2 = document.getElementById("pentagon-input2").value;
+    if (isNaN(input1) || isNaN(input2) || input1=="" || input2=="") {
+        alert("Kindly enter a valid input");
+    }
+    else{
+        var result = (0.5*input1*input2).toFixed(2);
+        var p = document.createElement("p");
+        p.textContent = count+". Pentagon: "+result+"cm2";
+        document.getElementById("result-results").appendChild(p);
+        count++;
+    }
+}
+
+
+function calculateEllipse(){
+    let input1 = document.getElementById("ellipse-input1").value;
+    let input2 = document.getElementById("ellipse-input2").value;
+    if (isNaN(input1) || isNaN(input2) || input1=="" || input2=="") {
+        alert("Kindly enter a valid input");
+    }
+    else{
+        var result = (3.1416*input1*input2).toFixed(2);
+        var p = document.createElement("p");
+        p.textContent = count+". Ellipse: "+result+"cm2";
+        document.getElementById("result-results").appendChild(p);
+        count++;
+    }
+}
